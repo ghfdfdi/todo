d5d4087be8919a4d623f665dc3542b2f27470dfb
YrGrabko <yaroslav.grabko@outlook.com>
2026-04-19 22:05:06 +0300

    первый коммит

---
+++ script1.py
@@ -0,0 +1,53 @@
+tasks = []
+while True:
+    print("---TODO СПИСОК---")
+    print("1. покказать задачи")
+    print("2. добавить задачу")
+    print("3. удалить задачу")
+    print("4. выйти")
+    try:
+        choice = int(input("выбери действие "))
+    except:
+        print("введите число")
+        continue
+
+
+    if choice == 1:
+        if not tasks:
+            print("список задач пуст")
+        else:
+            print("ваши задачи :")
+            for i, task in enumerate(tasks, start=1):
+                print(i, task)
+    elif choice == 2:
+
+        task = input("введите задачу ")
+        if task == "":
+            print("задача пустая напиши че нибудь")
+        else:
+            tasks.append(task)
+            print("задача добавленна")
+    elif choice == 3 :
+        if not tasks:
+            print("задач нет")
+        else:
+
+            for i, task in enumerate(tasks, start=1):
+                print(i, task)
+                num = int(input("введите задачи джля ужалениЯ"))
+
+                if 0 < num <= len(tasks):
+                    tasks.pop(num - 1)
+                    print("задача удалена")
+    elif choice == 4:
+        print("пока")
+        break
+    else:
+        print("неверный номер")
+
+
+
+
+
+
+
